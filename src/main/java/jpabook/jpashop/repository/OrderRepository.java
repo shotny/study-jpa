a0f2cbc0606dc18d1940d360fde8143f86e629c8
shotny <yeahh2483@gmail.com>
2023-01-12 17:39:22 +0900

    feat: 웹 계층

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,8 +3,12 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -20,8 +24,83 @@ public class OrderRepository {
     public Order findOne(Long orderId) {
         return em.find(Order.class, orderId);
     }
-/*
-    public List<Order> findAll(OrderSearch orderSearch) {
 
-    }*/
+    //검색 로직-동적쿼리
+//    public List<Order> findAll(OrderSearch orderSearch) {
+
+        /* status, name의 상태체크 x
+        em.createQuery("select o from Order o join o.member m" +
+                        "where o.status = :status" +
+                        "and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setFirstResult(10) //pagination - 10개씩 최대 100건
+                .setMaxResults(100)
+                .getResultList();
+        */
+
+
+        /*
+        //동적쿼리 생성 방법 1. jpql을 문자로 생성 -> 번거롭고 실수로인한 버그가 발생할 가능성 높음
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(100);
+        //orderStatus 있으면
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        // memberName 있으면
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+        */
+//    }
+
+    //동적쿼리 생성 방법 2. JPA 제공 표준 동적쿼리 빌더 "criteria" -> 유지보수 힘듦
+    public List<Order> findAllByCriteria(OrderSearch orderSearch){
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.equal(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(100);
+        return query.getResultList();
+    }
 }
